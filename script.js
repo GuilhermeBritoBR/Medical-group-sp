df536c13a57f53bf5e3feb3c704ef3b499995941
Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-06-07 14:18:30 -0300

    1.3

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ if ( senha.value == dados.senha[0])
     window.alert("senha ou usuário incorretos");
 }
 }
+function consulta()    {
+    var paciente = document.getElementById("paciente").value;
+    var dor = document.getElementById("dor").value;
+    var dia = document.getElementById("dia").value;
+    var especialidade = document.getElementById("especialidade").value;
+    var marcou = "Você marcou uma consulta no dia " + dia + "com a " + especialidade + "!";
+    window.alert("Você marcou uma consulta no dia " + dia + "com a " + especialidade + "!");
+    localStorage.setItem("marcou", marcou);
+    localStorage.setItem('dia', dia);
+   localStorage.setItem('paciente', paciente);
+    localStorage.setItem('dor', dor);
+  localStorage.setItem('especialidade', especialidade);
+    
+    }
+
+    function vil()  {
+        
+        window.alert("Veja as consultas marcadas!");
+        var paciente = localStorage.getItem("paciente");
+        var dor = localStorage.getItem("dor");
+        var especialidade = localStorage.getItem("especialidade");
+        var dia = localStorage.getItem("dia");
+        var resultadotext = 'O ' + paciente + ' Marcou consulta no dia ' + dia + " com a " + especialidade + ' por estar com dor ' + dor;
+      var resultado = document.getElementById("resultado_consulta");
+      resultado.innerText = resultadotext;
+       
+       
+    }
