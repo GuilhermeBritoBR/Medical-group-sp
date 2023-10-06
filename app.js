38d223740413d6693ddb979aaee6c589ec5b6e7d
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-10-06 15:45:55 -0300

    0.3

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-var mysql = require("mysql");
+
 var express = require("express");
+var mysql = require('mysql');
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+
+});
+app.use(express.static(__dirname+'/'));
+
+app.listen(port, () => {
+console.log(`Servidor Express está rodando na porta ${port}`);
+});
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "phpmyadmin",
+    password: "flamengo"
+  });
  
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
